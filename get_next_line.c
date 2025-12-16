ae3da4d7d70f504691c8d8aadaef744a3721b90e
Khalil Riad <kriad@e1r3p14.1337.ma>
2025-12-16 20:12:19 +0100

    edit

--- get_next_line.c
+++ get_next_line.c
@@ -1,83 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kriad <kriad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 17:08:49 by kriad             #+#    #+#             */
+/*   Updated: 2025/11/24 17:45:11 by kriad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-static char *read_and_join(int fd, char *stash)
+static char	*gnl_read(int fd, char *stash)
 {
-    char buffer[BUFFER_SIZE + 1];
-    char *tmp;
-    int byte = 1;
+	char	*buffer;
+	char	*tmp;
+	int		byte;
 
-    while (!ft_strchr(stash, '\n') && (byte = read(fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        buffer[byte] = '\0';
-        tmp = stash;
-        stash = ft_strjoin(tmp, buffer);
-        free(tmp);
-        if (!stash)
-            return (NULL);
-    }
-    if (byte < 0)
-    {
-        free(stash);
-        return (NULL);
-    }
-    return (stash);
+	byte = 1;
+	buffer = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(stash, '\n'))
+	{
+		byte = read(fd, buffer, BUFFER_SIZE);
+		if (byte <= 0)
+			break ;
+		buffer[byte] = '\0';
+		tmp = stash;
+		stash = ft_strjoin(tmp, buffer);
+		free(tmp);
+		if (!stash)
+			return (free(buffer), NULL);
+	}
+	free(buffer);
+	if (byte < 0)
+		return (free(stash), NULL);
+	return (stash);
 }
 
-static char *extract_line(char *stash)
+static char	*gnl_extract(char *stash)
 {
-    int i = 0;
+	int	i;
 
-    if (!stash || !stash[0])
-        return (NULL);
-
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    if (stash[i] == '\n')
-        i++;
-
-    return ft_substr(stash, 0, i);
+	i = 0;
+	if (!stash || !stash[0])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	return (ft_substr(stash, 0, i));
 }
 
-static char *clean_stash(char *stash)
+static char	*gnl_clean(char *stash)
 {
-    int i = 0;
-    char *new_stash;
+	char	*new;
+	int		i;
 
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    if (!stash[i])
-    {
-        free(stash);
-        return (NULL);
-    }
-    new_stash = ft_strdup(stash + i + 1);
-    free(stash);
-    return new_stash;
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	new = ft_strdup(stash + i + 1);
+	free(stash);
+	return (new);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *stash;
-    char *line;
+	static char	*stash;
+	char		*line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-
-    if (!stash)
-        stash = ft_strdup("");
-
-    stash = read_and_join(fd, stash);
-    if (!stash)
-        return (NULL);
-
-    line = extract_line(stash);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!stash)
+		stash = ft_strdup("");
+	stash = gnl_read(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = gnl_extract(stash);
 	if (!line)
 	{
-	    free(stash);
-	    stash = NULL;
-	    return (NULL);
+		free(stash);
+		stash = NULL;
+		return (NULL);
 	}
-    stash = clean_stash(stash);
-
-    return (line);
+	stash = gnl_clean(stash);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kriad <kriad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 17:27:51 by kriad             #+#    #+#             */
+/*   Updated: 2025/11/24 17:35:01 by kriad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,11 +20,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char    *get_next_line(int fd);
-char    *ft_strjoin(const char *s1, const char *s2);
-char    *ft_strdup(const char *s);
+char	*get_next_line(int fd);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
-size_t  ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 
 #endif
